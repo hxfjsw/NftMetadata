6804ddbe216fe6e1f08341904ec2e1e0caf9dace
eplktocelqm419 <122636167+eplktocelqm419@users.noreply.github.com>
2023-06-13 15:42:17 +0800

    1

---
+++ docs/frog/fix.js
@@ -0,0 +1,14 @@
+//读取images目录下的所有文件
+
+var fs = require('fs');
+
+var files = fs.readdirSync('./images');
+
+console.log(files);
+
+for (let i = 0; i < files.length; i++) {
+    let file = files[i];
+    let oldPath = './images/' + file;
+    let newPath = './images/' + i+'.png';
+    fs.renameSync(oldPath, newPath);
+}
---
+++ docs/frog/make_data.js
@@ -0,0 +1,6 @@
+for (let i = 0; i < 100; i++) {
+    let metadata = {
+      "image": "images/" + i%13 + ".png",
+      "name": "Frog #" + i,
+    };
+}
